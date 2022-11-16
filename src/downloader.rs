5265d80ca8f35dda03a253351c62541d35cf2c21
Ofer Sadan <ofersadan85@gmail.com>
2022-11-16 09:11:25 +0000

    prep for downloader

---
+++ src/downloader.rs
@@ -0,0 +1,2 @@
+// ref 1: https://github.com/mihaigalos/tutorials/blob/master/rust/download_with_progressbar/src/main.rs
+// ref 2: https://gist.github.com/giuliano-oliveira/4d11d6b3bb003dba3a1b53f43d81b30d
