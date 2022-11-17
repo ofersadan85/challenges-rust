3ccfcab9e2d0670c4ce0bbec96f52decd06b7099
Ofer Sadan <ofersadan85@gmail.com>
2022-11-17 12:45:30 +0200

    consolidated maths with primes

--- src/maths.rs
+++ src/maths.rs
@@ -1,3 +1,52 @@
+use std::collections::HashMap;
+
+pub fn is_prime(n: u128) -> bool {
+    let max_div: u128 = (n as f64).sqrt() as u128 + 1;
+    for div in 2..max_div {
+        if n % div == 0 {
+            return false;
+        }
+    }
+    true
+}
+
+pub fn prime_factors(n: u128) -> Vec<u128> {
+    let mut n: u128 = n;
+    let mut div: u128 = 2;
+    let mut result: Vec<u128> = Vec::new();
+    let max_div: f64 = (n as f64).sqrt();
+    while n > 1 {
+        if div > max_div as u128 {
+            result.push(n);
+            break;
+        } else if n % div == 0 {
+            result.push(div);
+            n = n / div;
+            div = 2;
+        } else {
+            div += 1;
+        }
+    }
+    result
+}
+
+pub fn gcd(numbers: Vec<u128>) -> u128 {
+    // returns the greatest common divisor of n numbers
+    if numbers.len() == 1 {
+        return numbers[0];
+    }
+    let a = numbers[0];
+    let b = gcd(numbers[1..].to_vec());
+    gcd_of_two_numbers(a, b)
+}
+
+fn gcd_of_two_numbers(a: u128, b: u128) -> u128 {
+    if b == 0 {
+        return a;
+    }
+    gcd_of_two_numbers(b, a % b)
+}
+
 pub fn factorial(n: usize) -> u128 {
     (1..=n).reduce(|a, b| a * b).unwrap_or(1) as u128
 }
@@ -28,4 +77,21 @@ pub fn pascals_triangle_2(n: usize) -> Vec<usize> {
         rows.push(next);
     }
     rows.into_iter().flatten().collect::<Vec<usize>>()
+}
+
+
+pub fn is_perfect_power(n: u64) -> Option<(u64, u32)> {
+    let mut factor_map: HashMap<u128, u128> = HashMap::new();
+    for f in prime_factors(n as u128) {
+        factor_map
+            .entry(f)
+            .and_modify(|counter| *counter += 1)
+            .or_insert(1);
+    }
+    let exp = gcd(factor_map.values().cloned().collect());
+    if exp > 1 {
+        Some(((n as f64).powf(1.0 / exp as f64).round() as u64, exp as u32))
+    } else {
+        None
+    }
 }
